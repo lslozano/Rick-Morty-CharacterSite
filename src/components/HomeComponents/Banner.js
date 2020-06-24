b9d1a3c68d9d1d35e6576ebd6f842a32508dc2c3
Luis Lozano <lslozano11@gmail.com>
2020-06-23 19:35:57 -0500

    Banner ready
    Banner for the site ready.

---
+++ src/components/HomeComponents/Banner.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { Button, Container, Col, Row } from 'react-bootstrap';
+import '../../styles/banner.css';
+
+function Banner() {
+  return (
+    <>
+    <Container fluid>
+      <section id="banner">
+        <Row className="banner-row">
+          <Col className="cta" xs={12} md={6}>
+            <h1 className="big-heading">All Rick and Morty characters in one place!</h1>
+            <Link to="/characters">
+              <Button variant="info">Search characters</Button>
+            </Link>
+          </Col>
+          <Col xs={12} md={6}>
+           <img className="rick-morty-logo" src="https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-portal-shoes-white-clothing-zavvi-23.png" alt="Rick and Morty Logo" />        
+          </Col>
+        </Row>
+      </section>
+    </Container>
+    </>
+  )
+}
+
+export default Banner
