58586f48e59c8b594a44208e3718c5a117906d1f
Luis Lozano <lslozano11@gmail.com>
2020-06-25 19:22:34 -0500

    Basic setup of the footer
    Basic setup of the footer ready. Next up will come positioning and some style.

---
+++ src/components/HomeComponents/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <p>Rick and Morty trademark and all it's characters belong to adulswim. <br />
+      This is only a site to demostrate the use of certain technologies.
+      </p>
+      <h3>Site created by Luis Lozano.</h3>
+      <p>For any inquiries, please contact me at:</p>
+      <i class="fab fa-github-square"></i>
+      <i class="fab fa-linkedin"></i>
+      <i class="fab fa-medium"></i>
+    </div>
+  )
+}
+
+export default Footer
