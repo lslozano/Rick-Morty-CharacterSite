0a270c56d3e94922630b644a835603bcc336ae05
Luis Lozano <lslozano11@gmail.com>
2020-06-25 22:48:45 -0500

    Footer ready, without any styles
    The footer is ready but it needs styles. Working on them through Responsive-Design branch.

--- src/components/HomeComponents/Footer.js
+++ src/components/HomeComponents/Footer.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../../styles/footer.css';
 
 function Footer() {
   return (
-    <div>
-      <p>Rick and Morty trademark and all it's characters belong to adulswim. <br />
-      This is only a site to demostrate the use of certain technologies.
-      </p>
+    <div id="footer">
       <h3>Site created by Luis Lozano.</h3>
       <p>For any inquiries, please contact me at:</p>
-      <i class="fab fa-github-square"></i>
-      <i class="fab fa-linkedin"></i>
-      <i class="fab fa-medium"></i>
+      <a href="https://github.com/lslozano" rel="noopener noreferrer" target="_blank"><i class="fab fa-github-square"></i></a>
+      <a href="https://www.linkedin.com/in/lslozano/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin"></i></a>
+      <a href="https://medium.com/@lslozano" rel="noopener noreferrer" target="_blank"><i class="fab fa-medium"></i></a>
+      <p className="disclaimer">Disclaimer: Rick and Morty trademark and all it's characters belong to adultswim. <br />
+      This is only a site to demostrate the use of certain technologies.
+      </p>
     </div>
   )
 }
