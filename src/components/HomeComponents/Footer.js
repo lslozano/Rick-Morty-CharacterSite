1ca78a1883f3d68d61e72d04f66741a0a832d3f5
Luis Lozano <lslozano11@gmail.com>
2020-06-26 20:57:41 -0500

    Styles of Home ready and full responsive

--- src/components/HomeComponents/Footer.js
+++ src/components/HomeComponents/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
     <div id="footer">
       <h3>Site created by Luis Lozano.</h3>
       <p>For any inquiries, please contact me at:</p>
-      <a href="https://github.com/lslozano" rel="noopener noreferrer" target="_blank"><i class="fab fa-github-square"></i></a>
-      <a href="https://www.linkedin.com/in/lslozano/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin"></i></a>
-      <a href="https://medium.com/@lslozano" rel="noopener noreferrer" target="_blank"><i class="fab fa-medium"></i></a>
+      <a href="https://github.com/lslozano" rel="noopener noreferrer" target="_blank"><i class="fab fa-github-square contact-icon"></i></a>
+      <a href="https://www.linkedin.com/in/lslozano/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin contact-icon"></i></a>
+      <a href="https://medium.com/@lslozano" rel="noopener noreferrer" target="_blank"><i class="fab fa-medium contact-icon"></i></a>
       <p className="disclaimer">Disclaimer: Rick and Morty trademark and all it's characters belong to adultswim. <br />
       This is only a site to demostrate the use of certain technologies.
       </p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import '../styles/navbar.css';
-import '../styles/links.css';
+import '../styles/navlinks.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav>
-      <Link to="/" className="router-link">
+      <Link to="/" className="link">
         <h3>Rick and Morty</h3>
       </Link>
       <ul className="nav-links">
-        <Link to="/about" className="router-link">
+        <Link to="/about" className="link">
           <li>About</li>
         </Link>
-        <Link to="/characters" className="router-link">
+        <Link to="/characters" className="link">
           <li>Characters</li>
         </Link>
-        <Link to="/characters" className="router-link">
+        <Link to="/characters" className="link">
           <li>Contact</li>
         </Link>
       </ul>
