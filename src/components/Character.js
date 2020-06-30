7f2fd404983de66ffd40e1d95cac7de55dc2db8a
Luis Lozano <lslozano11@gmail.com>
2020-06-29 20:51:49 -0500

    Character page with responsive design ready

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Footer from './HomeComponents/Footer';
 import NotFound from './NotFound';
+import '../styles/character.css';
 import '../styles/notfound.css';
 
 // useState - Holds the information we are getting from an API. 
@@ -28,26 +29,28 @@ function Character({ match }) {
     )
   } else {
     return (
-      <>
-      <Card className="text-center" key={character.id}>
-      <Card.Img variant="top" src={character.image} />
-        <Card.Body>
-          <Card.Title>{character.name}</Card.Title>
-          <Card.Text>
-            Gender: {character.gender} <br />
-            Species: {character.species} <br />
-            Origin: {character.origin.name} <br />
-            Location: {character.location.name} <br />
-            Status: {character.status}
-            Number of Episodes: {character.episode.length}
-          </Card.Text>
-          <Button variant="primary">
-            <Link to={`/characters/${character.id}`}>Details</Link>
-          </Button>
-        </Card.Body>
-      </Card>
-      <Footer />
-      </>
+      <section id="character">
+        <Container fluid>
+          <Row>
+            <Col className="character-img" xs={12} md={6}>
+              <img src={character.image} alt={character.name} />        
+            </Col>
+            <Col className="character-info" xs={12} md={6}>
+              <h1>{character.name}</h1>
+              <p>Gender: {character.gender}</p><br />
+              <p>Species: {character.species}</p> <br />
+              <p>Origin: {character.origin.name}</p> <br />
+              <p>Location: {character.location.name}</p> <br />
+              <p>Status: {character.status}</p> <br />
+              <p>Number of Episodes: {character.episode.length}</p>
+              <Link to="/characters">
+                <Button variant="info">Back characters</Button>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
+    </section>
     )
   }
 }
