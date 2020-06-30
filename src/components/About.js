46f53bdbeb1f2a208409d2a75ef331132c3d645e
Luis Lozano <lslozano11@gmail.com>
2020-06-29 21:50:46 -0500

    Basic styles set

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import Footer from './HomeComponents/Footer';
+import '../styles/about.css';
+
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <>
+    <section id="about"> 
+      <h3>This project was built using:</h3>
+      <ul className="tools-used">
+        <li>Single responsability principle</li>
+        <li>Separation of concerns</li>
+        <li>Reusable components</li>
+        <li>Reusable styles</li>
+        <li>React Hooks - useEffect, useState</li>
+        <li>Mobile first design approach</li>
+        <li>Conditional rendering</li>
+        <li>Full use of version control via Github: Different branches, commits, pull requests and merges.</li>
+        <li>HTML</li>
+        <li>Javascript</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>React Bootstrap</li>
+      </ul>
+      <p>This site is full responsive. It has been verified using the mobile friendly google test.</p>
+      <p>This site has an overall performance score above 95, obtained via lighthouse.</p> 
+    </section>
+    <Footer />
+    </>
   );
 }
 
--- src/components/HomeComponents/Banner.js
+++ src/components/HomeComponents/Banner.js
@@ -5,9 +5,8 @@ import '../../styles/banner.css';
 
 function Banner() {
   return (
-    <>
-    <Container fluid id="banner">
-      <section>
+    <section>
+      <Container fluid id="banner">
         <Row className="banner-row">
           <Col className="cta" xs={12} md={6}>
             <h1 className="big-heading">All Rick and Morty characters in one place!</h1>
@@ -16,12 +15,11 @@ function Banner() {
             </Link>
           </Col>
           <Col xs={12} md={6}>
-           <img className="rick-morty-logo" src="https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-portal-shoes-white-clothing-zavvi-23.png" alt="Rick and Morty Logo" />        
+            <img className="rick-morty-logo" src="https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-portal-shoes-white-clothing-zavvi-23.png" alt="Rick and Morty Logo" />        
           </Col>
         </Row>
-      </section>
     </Container>
-    </>
+   </section>
   )
 }
 
